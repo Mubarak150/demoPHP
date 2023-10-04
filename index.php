a72eaa1fbdb0e30c785cd719f1de639176230a17
Mubarak150 <mkmubarak2347@gmail.com>
2023-10-04 05:58:44 -0700

    video 18; made the code for accessing db more dynamic and created a file Database.php and separated the class from index

--- index.php
+++ index.php
@@ -1,34 +1,12 @@
 <?php
 
 require "functions.php";
-// require "router.php";
+require "Database.php";
 
-// connection to the database;
-// declaring  a class
-// class Person {
-//     public $name;
-//     public $age; 
 
-//     public function breathe () {
-//         echo $this->name . ' is breathing!';
-//     }
-// }
+$db = new Database();
+$posts = $db->query("select * from posts")-> fetchAll(PDO::FETCH_ASSOC);
 
-// // adding elements to the class
-// $person = new Person ();
-// $person-> name = 'john doe';
-// $person -> age = 25; 
-
-// $person->breathe ();
-
-$dsn = "mysql:host=localhost;port=3306;dbname=Demo2;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement -> execute();
-
-$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
 
 foreach($posts as $post) {
     echo '<li>' . $post['title'] . "</li>";
