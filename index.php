8bdee94b754d66604a70fcdfb9f0624064e44ec9
Mubarak150 <mkmubarak2347@gmail.com>
2023-10-06 10:04:31 -0700

    video 20 completed

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ require "Database.php";
 
 $config = require('config.php');
 $db = new Database($config['database']);
-$posts = $db->query("select * from posts");
 
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+$posts = $db->query($query, [$id])->fetch();
+dd($posts);
 
-foreach($posts as $post) {
-    echo '<li>' . $post['title'] . "</li>";
-}
+// foreach($posts as $post) {
+//     echo '<li>' . $post['title'] . "</li>";
+// } //thapa production css
