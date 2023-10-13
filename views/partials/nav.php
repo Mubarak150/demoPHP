23252269b05e667740fb7ba671857d6c9e3c791d
Mubarak150 <mkmubarak2347@gmail.com>
2023-10-13 13:08:46 -0700

    nn

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -12,7 +12,7 @@
               <a href="about" class="<?= urlIs ('/demoPHP/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>  hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About Us</a>
               <a href="/demoPHP/notes" class="<?= urlIs ('/demoPHP/notes') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>  hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
               <a href="contact" class="<?= urlIs ('/demoPHP/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contacts</a>
-            </div>
+            </div> 
           </div>
         </div>
         <div class="hidden md:block">
